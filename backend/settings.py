70fa2edb2a8b9acd486ecc0c9b87153d438dae82
Shoaib <shoaibshoukatbhatti6252@gmail.com>
2026-03-21 19:36:07 +0500

    add changes

--- backend/settings.py
+++ backend/settings.py
@@ -139,7 +139,9 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:5174",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
     "https://cornflourfrontend.vercel.app",
 ]
 
